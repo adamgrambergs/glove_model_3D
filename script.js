7941306325a8b42234a04696eb3d7873ae5ee524
adamgrambergs <adamgrambergs@gmail.com>
2025-05-17 11:28:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Load the 3D glove model
-loader.load('https://adamgrambergs.github.io/glove_model_3D/gloveModel1.glb', function (glove) {    
+loader.load('https://adamgrambergs.github.io/glove_model_3D/gloveModel.glb', function (glove) {    
     console.log("Model loaded successfully!");
     scene.add(glove.scene);
 }, undefined, function (error) {
