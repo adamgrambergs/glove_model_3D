b66ad8bcebaa12d92f0000b84385c4cf9065b837
adamgrambergs <adamgrambergs@gmail.com>
2025-05-17 09:22:47 -0700

    Update script.js
    remove three.js code

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("gloveCanvas") });
-const loader = new THREE.GLTFLoader();
-
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(5, 5, 5);
-scene.add(directionalLight);
-
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
